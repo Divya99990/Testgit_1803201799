f73cc6500a8e2976607a3e04a125f08199646fe8
Divya99990 <divya.odi0@gmail.com>
2017-03-29 14:44:45 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello {
+      public static void main(String[] args) {
+            System.out.println("I am about to print \"Hello!\" ten times.");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Hello!");
+            System.out.println("Uff... It was long. Is there a way to compact repeating things?");
+      }
+}
